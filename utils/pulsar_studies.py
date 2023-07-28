157078d6304919189629921916d8be9a3a5c002b
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2023-07-28 11:38:00 +0530

    Update pulsar_studies.py

--- utils/pulsar_studies.py
+++ utils/pulsar_studies.py
@@ -1,50 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[1]:
-
-
+#A HARD CODED PRIMITIVE script TO SHOW HOW PULSAR DATA IS ANALYSED BY SIGPYPROC3
 import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
 from sigpyproc.readers import FilReader
 from scipy.fft import fft
-get_ipython().run_line_magic('matplotlib', 'inline')
-
-
-# In[2]:
-
 
 #Load the converted fil file
 filfile_path=Path('/DATA/chime-slow/raw/filterbank/2022/06/18/0120/016ms/1655547755_1655551360_beam_0120_016ms.fil')
-#Path('/home/kevin/kevinlukechime/SLOWTRANSIENT/TESTING/clustering/filterbanks/spliced_blc4041424344454647_guppi_59103_01372_PSR_B2021+51_0009.rawspec.0002.fil')
 fil_file=FilReader(filfile_path.as_posix())
 fil_file.header
 
-
-# In[148]:
-
-
-#Use sigpyproc3 tools to dedisperse and downsample the data
+#Perform dedispersion and downsampling
 tdownsamp=1
 subband=2
 DM=140
-
 data = fil_file.read_block(0, fil_file.header.nsamples)
 data = data.dedisperse(DM)
 data = data.downsample(tdownsamp, subband)
 data = data.normalise()
 tseries = data.get_tim()
-
-
-# In[4]:
-
-
 data.shape    
-
-
-# In[227]:
-
 
 #Fold timeseries to get the pulse
 from sigpyproc.timeseries import TimeSeries
