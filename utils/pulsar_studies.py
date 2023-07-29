59999c910f1fb808760163da2c25ae887a1b70f9
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2023-07-29 14:15:03 +0530

    Update pulsar_studies.py

--- utils/pulsar_studies.py
+++ utils/pulsar_studies.py
@@ -1,57 +1,52 @@
 #!/usr/bin/env python
 # coding: utf-8
-#A HARD CODED PRIMITIVE script TO SHOW HOW PULSAR DATA IS ANALYSED BY SIGPYPROC3
+
 import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
 from sigpyproc.readers import FilReader
 from scipy.fft import fft
+get_ipython().run_line_magic('matplotlib', 'inline')
+
 
 #Load the converted fil file
-filfile_path=Path('/DATA/chime-slow/raw/filterbank/2022/06/18/0120/016ms/1655547755_1655551360_beam_0120_016ms.fil')
+filfile_path=Path('filterbanks/1655547760_1655551362_beam_1134_016ms.fil')
 fil_file=FilReader(filfile_path.as_posix())
 fil_file.header
 
-#Perform dedispersion and downsampling
-tdownsamp=1
-subband=2
-DM=140
-data = fil_file.read_block(0, fil_file.header.nsamples)
-data = data.dedisperse(DM)
-data = data.downsample(tdownsamp, subband)
-data = data.normalise()
-tseries = data.get_tim()
-data.shape    
 
-#Fold timeseries to get the pulse
-from sigpyproc.timeseries import TimeSeries
-def folder(tseries, fil_file, period, nbin,nint):
-    
-    series=TimeSeries(tseries, fil_file.header)
-    folded=series.fold(period, nbins=nbin, nints=nint)
-    profile=folded.get_profile()
-    waterfall=folded.get_time_phase()
-    
-    
-    plt.figure(figsize=(7,5))
-    
-    plt.subplot(2,2,1)
-    plt.title('Normal dedispersed timeseries')
-    plt.xlabel('samples')
-    plt.ylabel('Power, arbitrary units')
-    plt.plot(tseries)
-    
-    plt.subplot(2,2,2)
-    plt.title('Folded timeseries of a pulsar')
-    plt.xlabel('Phase bins')
-    plt.ylabel('Power, arbitrary units')
-    plt.plot(profile)
-    plt.axis('off')
-    
-    plt.subplot(2,2,4)
-    plt.xlabel('Phase bin')
-    plt.imshow(waterfall, aspect='auto', cmap='hot', vmax=10, vmin=-10)
-    
-    
-folder(tseries, fil_file, 0.4, 25,100)    
+data=fil_file
+freq=1.89 #Hz FROM ATNF
+DM=22.54 #FROM ATNF
+bins=100 
+ints=20
+bands=128
 
+#Fold the data
+folded=data.fold(period=1/freq,dm=DM,accel=0 , 
+                 nbins=bins, nints=ints,
+                 nbands=bands)
+
+print('Shape of folded data',folded.shape)
+#Get the waterfall
+waterfall=folded.get_freq_phase()
+#Get the profile 
+profile=folded.get_profile()
+print('Shape of watefall',waterfall.shape)
+
+plt.figure(figsize=(4,6))
+
+plt.subplot(2,1,1)
+plt.title('Folded Plot of PSR B2021+51')
+plt.xlabel('Phase bins')
+plt.ylabel('Power arbitrary units')
+plt.plot(profile[:])
+
+plt.subplot(2,1,2)
+plt.xlabel('Phase bins')
+plt.ylabel('Frequency bands')
+plt.imshow(waterfall[:,:], aspect='auto', cmap='hot', 
+           vmax=max(np.nanmedian(waterfall, axis=0)), 
+           vmin=min(np.nanmedian(waterfall, axis=0)))
+
+plt.tight_layout()
