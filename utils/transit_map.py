a67decd6bef6789b7969c6d64a7ea1010f30753e
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2023-10-10 20:04:35 +0530

    Update transit_map.py

--- utils/transit_map.py
+++ utils/transit_map.py
@@ -8,46 +8,40 @@ plt.style.use(astropy_mpl_style)
 quantity_support()
 from astropy.coordinates import get_body
 
+
 loc = EarthLocation(lat=19*u.deg, lon=72.8*u.deg, height=0*u.m)
 utcoffset = 5.5*u.hour
-time_mid = Time('2023-10-9 16:00:00', scale='utc')+ utcoffset
-
-
-
-time_delta = np.linspace(-6,12, 20)*u.hour
+time_mid = Time('2023-10-11 16:00:00', scale='utc')+ utcoffset
+time_delta = np.linspace(-12,12, 20)*u.hour
 time_frame = time_mid + time_delta
 time_frame_alt_az = AltAz(obstime=time_frame, location=loc)
-
-from astropy.coordinates import get_body
 
 moon_path = get_body("moon", time_frame)
 moon_path_alt_az = moon_path.transform_to(time_frame_alt_az)
 
-az_moon=np.deg2rad(moon_path_alt_az.az)
-alt_moon=moon_path_alt_az.alt
+az_moon=np.deg2rad(moon_path_alt_az.az[moon_path_alt_az.alt>0])
+alt_moon=(moon_path_alt_az.alt[moon_path_alt_az.alt>0])
 
-
+plt.figure(figsize=(20,20), dpi=100)  
 ax = plt.subplot(1, 1, 1, projection='polar')
-
 ax.set_rlim(90, 0, 1)
-ax.scatter(az_moon,alt_moon, marker='*')
+ax.scatter(az_moon,alt_moon, marker='o', s=500)
 
 ax.set_theta_zero_location('N')  # North as 0 degrees
 ax.set_theta_direction(-1)  # Clockwise direction
+ax.set_xticklabels(['0°', '45°', '90°', '135°', '180°', '225°', '270°', '315°'], fontsize=20)
+ax.set_rlabel_position(200)  # Position of radial labels
 
-#ax.set_rlabel_position(90)  # Position of radial labels
 #Time string
 obstime=[time.value for time in moon_path_alt_az.obstime[moon_path_alt_az.alt>0]]
-#Print the rise time and transit times with their altitudes
-for alt, time in zip(moon_path_alt_az.alt[moon_path_alt_az.alt>0], 
-                     moon_path_alt_az.obstime[moon_path_alt_az.alt>0]):
 
-    print(alt, time)
+print("\n The obs time and corresponding altitudes are\n")
+for i, txt in enumerate(obstime):
+    ax.annotate(txt[11:16],(az_moon[i],alt_moon[i]), fontsize=20)
+    print(txt,alt_moon[i])
 
 # Add a title and legend
 plt.title('Moon Altitude vs Azimuth')
-
-
 # Show the plot
 plt.show()
 
