2a24e5c5ff4e839a0fd8875ddeb128b5f2c606ae
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2023-10-13 21:50:41 +0530

    Update transit_map.py

--- utils/transit_map.py
+++ utils/transit_map.py
@@ -7,63 +7,62 @@ from astropy.time import Time
 plt.style.use(astropy_mpl_style)
 quantity_support()
 from astropy.coordinates import get_body
-from matplotlib import gridspec
 
-
-def path_tracking(latitude, longitude, date):
-    
+def path_tracking(latitude, longitude, date, planet_string):
     loc = EarthLocation(lat=latitude*u.deg, lon=longitude*u.deg, height=0*u.m)
     utcoffset = 5.5*u.hour
-    time_mid = Time(date, scale='utc')+ utcoffset
-    time_delta = np.linspace(-12,12, 20)*u.hour
+    time_mid = Time(date, format='iso',scale='utc')+ utcoffset
+    time_delta = np.linspace(-10,10, 10)*u.hour
     time_frame = time_mid + time_delta
+    
+    object = SkyCoord.from_name(planet_string)
+    
     time_frame_alt_az = AltAz(obstime=time_frame, location=loc)
-    
-    moon_path = get_body("moon", time_frame)
-    moon_path_alt_az = moon_path.transform_to(time_frame_alt_az)
-    
-    az_moon=np.deg2rad(moon_path_alt_az.az[moon_path_alt_az.alt>0])
-    alt_moon=(moon_path_alt_az.alt[moon_path_alt_az.alt>0])
+    object_alt_az = object.transform_to(time_frame_alt_az)
+    mumbai_loc_obstime=object_alt_az.obstime + 8*u.hour
+    obstime=[time.value for time in mumbai_loc_obstime]
+    obstime_hrs=[hrs[11:16] for hrs in obstime]
+    az=np.deg2rad(object_alt_az.az)
+    alt=object_alt_az.alt
 
-    return az_moon, alt_moon, moon_path_alt_az
-    
+    return alt, az, obstime_hrs, obstime
 
-def visualise(az_moon, alt_moon, moon_path_alt_az, date):
-    
-    fig=plt.figure(figsize=(20,20), dpi=100)  
 
-    ax=fig.add_subplot(1,1,1, projection='polar')
+    
+def visualise(alt, az, obstime_hrs, obstime, planet_string):
+    fig=plt.figure(figsize=(10,15))  
+    
+    ax=fig.add_subplot(2,1,1, projection='polar')
     #ax = plt.subplot(1, 1, 1, projection='polar')
     ax.set_rlim(90, 0, 1)
-    ax.scatter(az_moon,alt_moon, marker='o', s=500)
+    ax.scatter(az,alt, marker='o', s=50)
     
     ax.set_theta_zero_location('N')  # North as 0 degrees
     ax.set_theta_direction(-1)  # Clockwise direction
-    ax.set_xticklabels(['0°', '45°', '90°', '135°', '180°', '225°', '270°', '315°'], fontsize=20)
+    ax.set_xticklabels(['0°', '45°', '90°', '135°', '180°', '225°', '270°', '315°'], fontsize=10)
     ax.set_rlabel_position(200)  # Position of radial labels
-    
-    #Time string
-    obstime=[time.value for time in moon_path_alt_az.obstime[moon_path_alt_az.alt>0]]
-    
-    print("\n The obs time and corresponding altitudes are\n")
-
-  
+    plt.title('{0:s}\n Altitude vs Azimuth\n on {1:s}'.format(planet_string, date))
     for i, txt in enumerate(obstime):
-        ax.annotate(txt[11:16],(az_moon[i],alt_moon[i]), fontsize=20)
-        print(txt,alt_moon[i])
+        ax.annotate(txt[11:16],(az[i],alt[i]), fontsize=10)
     
+    
+    ax1=fig.add_subplot(2,1,2)
+    ax1.scatter(obstime_hrs,alt )
+    for i, txt in enumerate(obstime):
+        ax1.annotate(txt[11:16],(obstime_hrs[i],alt[i]), fontsize=10)
+    
+        
     # Add a title and legend
-    plt.title('Moon Altitude vs Azimuth on {0:s}'.format(date))
+    plt.title('{0:s}\n Altitude vs time\n on {1:s}'.format(planet_string, date))
     # Show the plot
     fig.savefig('moon_map_{0:s}.png'.format(date))
-    
-    fig.show()
+
 
 if __name__ == "__main__":
-    latitude=19
-    longitude=72.8
-    date='2023-10-11 16:00:00'
-    
-    az_moon, alt_moon, moon_path_alt_az= path_tracking(latitude, longitude, date)
-    
-    visualise(az_moon, alt_moon, moon_path_alt_az, date)
+    latitude=19.3547
+    longitude=72.8328 
+    date='2023-10-14 12:00:00'
+    planet_string='jupiter'
+
+    alt, az, obstime_hrs, obstime= path_tracking(latitude, longitude, date, planet_string)
+    'visualise(alt, az, obstime_hrs, obstime, planet_string)
